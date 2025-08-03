11ecb3882147f1dd3a692eb5c44e3aec1c206807
desktop-tutorial <https://github.com/sumit6020/desktop-tutorial.git>
2025-08-03 19:26:24 +0530

    first commit on newBranch

--- first.java
+++ first.java
@@ -2,5 +2,6 @@ public class first {
     public static void main(String[]args){
         System.out.println("hello second batch");
         System.out.println("call the word");
+        System.out.println(".(additional bracket)");
     }
 }
